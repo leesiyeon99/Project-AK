eecd34b4ae581dd7af28a6cb6631839b33e575d9
LeeJaeHo-96 <towpam123@gmail.com>
2024-10-26 00:32:52 +0900

    Feat: Shiled On Off

--- Assets/LJH/Scripts/LJH_Shield.cs
+++ Assets/LJH/Scripts/LJH_Shield.cs
@@ -7,57 +7,82 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class LJH_Shield : MonoBehaviour
 {
-    [SerializeField] GameObject ObjShieldPrefab;
     [SerializeField] GameObject playerPos;
-    [SerializeField] InputActionReference shieldOn;
+    [SerializeField] InputActionReference shieldOnOff;
 
+    bool isSheild;
     bool isBreaked;
     float durability = 5;
 
     private void Start()
     {
-        ObjShieldPrefab = gameObject;
-        //shieldOn.action.performed += ShieldOn;
+        gameObject.SetActive(false);
 
+        isSheild = false;
         isBreaked = false;
         durability = 5;
     }
-    
+    // 방패가 활성화 될 때
+    private void OnEnable()
+    {
+        // 트리거 버튼에서 ShieldOn 제거
+        shieldOnOff.action.performed -= ShieldOn;
+        // 트리거 버튼에서 ShiledOff 추가
+        shieldOnOff.action.performed += ShieldOff;
+    }
+
+    // 방패가 비활성화 될 때
+    private void OnDisable()
+    {
+        // 트리거 버튼에서 ShieldOn 추가
+        shieldOnOff.action.performed += ShieldOn;
+        // 트리거 버튼에서 ShiledOff 제거
+        shieldOnOff.action.performed -= ShieldOff;
+    }
+
     private void Update()
     {
-        transform.position = playerPos.transform.position; // 쉴드의 위치는 플레이어 위치로 따라다니게
+        // 쉴드의 위치는 플레이어 위치로 따라다니게
+        transform.position = playerPos.transform.position;
         
     }
 
-   
 
-    public void ShieldOn()                                  // 방패 활성화
+    // 방패 활성화
+    public void ShieldOn(InputAction.CallbackContext obj)
     {
-        ObjShieldPrefab.SetActive(true);
+        gameObject.SetActive(true);
+        isSheild = true;
     }
 
-    public void ShieldOff()                                 // 방패 비활성화
+    // 방패 비활성화
+    public void ShieldOff(InputAction.CallbackContext obj)
     {
-        ObjShieldPrefab.SetActive(false);
+        gameObject.SetActive(false);
+        isSheild = false;
     }
 
     public void BreakedShield()
     {
-        ObjShieldPrefab.SetActive(false);
+        gameObject.SetActive(false);
         isBreaked = true;
     }
 
-    /*
+    
     IEnumerator ShieldCoolDown()
     {
-        yield return new WaitForSecond(2.0f);               // 2초간 방패 들기 불가
+        // 2초간 방패 들기 불가
+        yield return new WaitForSeconds(2.0f);
+
+        // 2초 뒤 RecoveryShield 코루틴 실행
         Coroutine recovery = StartCoroutine("RecoveryShield");
     }
 
     IEnumerator RecoveryShield()
     {
-        yield return new WaitForSecond(3.0f);               // 방패 들기 불가 + 3초 후 방패 수리 완료
+        // 방패 들기 불가 + 3초 후 방패 수리 완료
+        yield return new WaitForSeconds(3.0f);
         isBreaked = false;
     }
-    */
+    
 }
