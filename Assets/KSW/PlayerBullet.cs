95be339211616435f65652121cd5b9acde84a584
Z1758 <timeofwitch@gmail.com>
2024-10-25 17:12:25 +0900

    feat : 총알 오브젝트 풀 적용

--- Assets/KSW/PlayerBullet.cs
+++ Assets/KSW/PlayerBullet.cs
@@ -6,26 +6,53 @@ public class PlayerBullet : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidBody;
     [SerializeField] private float bulletSpeed;
+
+    private PlayerGun playerGun;
+
+    [SerializeField] private float returnDelay;
+
+    private WaitForSeconds returnWaitForSeconds;
+    private Coroutine returnCoroutine;
+
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        Fire();
+        returnWaitForSeconds = new WaitForSeconds(returnDelay);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveBullet()
     {
-        
-    }
-
-    public void Fire()
-    {
-
+        returnCoroutine = StartCoroutine(ReturnTime());
         rigidBody.velocity = transform.forward * bulletSpeed;
     
+    }
+
+    public void SetPlayerGun(PlayerGun _playerGun)
+    {
+        playerGun = _playerGun;
+    }
+  
+    // 오브젝트 풀 회수
+    private void ReturnBullet()
+    {
+        if (returnCoroutine != null)
+        {
+            StopCoroutine(returnCoroutine);
+        }
+
+        gameObject.SetActive(false);
+        rigidBody.velocity = Vector3.zero;
+        playerGun.EnqueueBullet(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ReturnBullet();
+    }
+
+    IEnumerator ReturnTime()
+    {
+        yield return returnWaitForSeconds;
+        ReturnBullet();
     }
 }
--- Assets/KSW/PlayerGun.cs
+++ Assets/KSW/PlayerGun.cs
@@ -8,21 +8,96 @@ public class PlayerGun : MonoBehaviour
 {
     [SerializeField] private Transform muzzle;
     [SerializeField] private InputActionReference shoot;
-    [SerializeField] private GameObject bullet;
+    
+    [SerializeField] private GameObject bulletPrefab;
+
+    [SerializeField] private float firingDelay;
+
+    [SerializeField] private int bulletPoolSize;
+
+    private Queue<PlayerBullet> playerBullets;
+
+    private WaitForSeconds firingWaitForSeconds;
+    private Coroutine firingCoroutine;
 
     private void OnEnable()
     {
         shoot.action.performed += OnShoot;
+        shoot.action.canceled += OffShoot;
     }
     private void OnDisable()
     {
-        shoot.action.performed += OnShoot;
+        shoot.action.performed -= OnShoot;
+        shoot.action.canceled -= OffShoot;
     }
- 
+
+    private void Awake()
+    {
+        playerBullets = new Queue<PlayerBullet>();
+        firingWaitForSeconds = new WaitForSeconds(firingDelay);
+       
+    }
+
+    private void Start()
+    {
+        SetBullet();
+    }
 
     void OnShoot(InputAction.CallbackContext obj)
     {
-        
-        Instantiate(bullet, muzzle.position, muzzle.rotation);
+        firingCoroutine = StartCoroutine(Firing());
+
+
     }
+    void OffShoot(InputAction.CallbackContext obj)
+    {
+        if (firingCoroutine != null)
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+
+    IEnumerator Firing()
+    {
+        while (true)
+        {
+            yield return firingWaitForSeconds;
+            Fire();
+           
+        }
+    }
+
+    // 총알 오브젝트 풀링
+    void SetBullet()
+    {
+        for (int i = 0; i < bulletPoolSize; i++)
+        {
+           GameObject bullet = Instantiate(bulletPrefab);
+           PlayerBullet playerBullet = bullet.GetComponent<PlayerBullet>();
+           playerBullets.Enqueue(playerBullet);
+           playerBullet.SetPlayerGun(this);
+           playerBullet.gameObject.SetActive(false);
+       
+        }
+    }
+
+    void Fire()
+    {
+   
+        if (playerBullets.Count <= 0)
+            return;
+
+        PlayerBullet playerBullet = playerBullets.Dequeue();
+        playerBullet.transform.position = muzzle.position;
+        playerBullet.transform.rotation = muzzle.rotation;
+        playerBullet.gameObject.SetActive(true);
+        playerBullet.MoveBullet();
+    }
+
+    public void EnqueueBullet(PlayerBullet playerBullet)
+    {
+        playerBullets.Enqueue(playerBullet);
+    }
+
 }
