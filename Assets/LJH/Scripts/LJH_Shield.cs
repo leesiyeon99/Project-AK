54626ac25591bc2b0d21ea8a6aa66bb3516c704a
LeeJaeHo-96 <towpam123@gmail.com>
2024-10-27 17:26:58 +0900

    Feat: 쉴드 리커버 방식 변경

--- Assets/LJH/Scripts/LJH_Shield.cs
+++ Assets/LJH/Scripts/LJH_Shield.cs
@@ -16,18 +16,23 @@ public class LJH_Shield : MonoBehaviour
     [Header("키입력")]
     [SerializeField] InputActionReference shieldOnOff;
     [SerializeField] InputActionReference damageTest; // 테스트 끝나고 지워야함
+    [SerializeField] InputActionReference fire;
+
+    [Header("오디오")]
+    [SerializeField] AudioSource damaged;
+    [SerializeField] AudioSource breaked;
 
     [Header("변수")]
-    public bool isShield;          // Comment: 역장 활성화 여부
-    public bool isBreaked;                // Comment: 역장 파괴 상태
+    public bool isShield;          // Comment: 역장 활성화 여부   필요없으면 삭제 예정
+    public bool isBreaked;         // Comment: 역장 파괴 상태
+    public bool isRecover;         // Comment: 회복 실행 여부
     public float durability;       // Comment: 역장 내구도
 
     private void Start()
     {
         
-
         gameObject.SetActive(false);
-
+        isRecover = false;
         isShield = false;
         isBreaked = false;
         durability = 5;
@@ -35,23 +40,41 @@ public class LJH_Shield : MonoBehaviour
     // Comment: 역장이 활성화 될 때
     private void OnEnable()
     {
-        // Comment: 트리거 버튼에서 ShieldOn 제거
-        shieldOnOff.action.performed -= ShieldOn;
-        // Comment: 트리거 버튼에서 ShiledOff 추가
-        shieldOnOff.action.performed += ShieldOff;
+        if (!isBreaked)
+        {
+            isRecover = false;
+            // Comment: 트리거 버튼에서 ShieldOn 제거
+            shieldOnOff.action.performed -= ShieldOn;
 
-        damageTest.action.performed += DamagedShield; // 테스트 끝나고 지워야함
+            // Comment: 트리거 버튼에서 ShiledOff 추가
+            shieldOnOff.action.performed += ShieldOff;
+
+            // Comment: 역장 활성화될 때 사격 기능 비활성화
+            //fire.action.performed -= GetComponent<PlayerInputWeapon>().OnFire;        총기와 연계 내용이라 머지 이후 주석처리 제거
+            //fire.action.performed -= Getcomponent<PlayerInputWeapon>().OffFire;
+
+            damageTest.action.performed += DamagedShield; // 테스트 끝나고 지워야함
+        }
     }
 
     // Comment: 역장이 비활성화 될 때
     private void OnDisable()
     {
-        // Comment: 트리거 버튼에서 ShieldOn 추가
-        shieldOnOff.action.performed += ShieldOn;
-        // Comment: 트리거 버튼에서 ShiledOff 제거
-        shieldOnOff.action.performed -= ShieldOff;
+        if (!isBreaked)
+        {
+            isRecover = true;
+            // Comment: 트리거 버튼에서 ShieldOn 추가
+            shieldOnOff.action.performed += ShieldOn;
 
-        damageTest.action.performed -= DamagedShield; // 테스트 끝나고 지워야함
+            // Comment: 트리거 버튼에서 ShiledOff 제거
+            shieldOnOff.action.performed -= ShieldOff;
+
+            // Comment: 역장 비활성화될 때 사격 기능 활성화
+            //fire.action.performed += GetComponent<PlayerInputWeapon>().OnFire;         총기와 연계 내용이라 머지 이후 주석처리 제거
+            //fire.action.performed += Getcomponent<PlayerInputWeapon>().OffFire;
+
+            damageTest.action.performed -= DamagedShield; // 테스트 끝나고 지워야함
+        }
     }
 
     private void Update()
@@ -63,13 +86,6 @@ public class LJH_Shield : MonoBehaviour
             {
                 BreakedShield();
             }
-        
-
-       // if () 
-       // {
-       //     DamagedShield();
-       // }
-
         
     }
 
@@ -92,8 +108,11 @@ public class LJH_Shield : MonoBehaviour
     // ToDo:    몬스터의 타격 방식에 따라 내용 변경 필요
     public void DamagedShield(InputAction.CallbackContext obj)// 인수 지워야함
     {
+        Debug.Log("역장 피해입음");
         // ToDo : 피격시 사운드 구현해야함
         durability -= 1;
+
+        damaged.Play();
         Debug.Log(durability);
     }
 
@@ -103,6 +122,8 @@ public class LJH_Shield : MonoBehaviour
         gameObject.SetActive(false);
         shieldRecover.SetActive(true);
         isBreaked = true;
+
+        breaked.Play();
         Debug.Log("역장이 파괴되었습니다.");
         
     }
--- Assets/LJH/Scripts/LJH_ShieldRecover.cs
+++ Assets/LJH/Scripts/LJH_ShieldRecover.cs
@@ -8,49 +8,93 @@ public class LJH_ShieldRecover : MonoBehaviour
     [SerializeField] GameObject shield;
 
     [Header("변수")]
-    float durability;
-    bool isBreaked;
+    [SerializeField] float durability;
+    [SerializeField] bool isBreaked;
+    [SerializeField] bool isRecover;
     void Start()
     {
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false); 활성화 상태 유지를 위해 삭제 예정
     }
 
-    void Update()
+    private void Update()
     {
-        // Comment: 역장 파괴 코루틴 실행
-        Coroutine braked = StartCoroutine(ShieldCoolDown());
-    }
+       durability = shield.GetComponent<LJH_Shield>().durability;
+       isBreaked = shield.GetComponent<LJH_Shield>().isBreaked;
+       isRecover = shield.GetComponent <LJH_Shield>().isRecover;
 
-    private void OnEnable()
-    {
-        durability = shield.GetComponent<LJH_Shield>().durability;
-        isBreaked = shield.GetComponent<LJH_Shield>().isBreaked;
-    }
+        if (isRecover)
+        {
+            Debug.Log("쉴드리커버생성");
+            //durability = shield.GetComponent<LJH_Shield>().durability;
+            //isBreaked = shield.GetComponent<LJH_Shield>().isBreaked;
 
+            // Comment: 역장이 파괴 상태일 때, 역장 파괴 쿨다운 코루틴 실행
+            if (isBreaked)
+            {
+                Coroutine breaked = StartCoroutine(ShieldCoolDown());
+                if (!isBreaked)
+                {
+                    Debug.Log("쉴드쿨다운코루틴종료");
+                    StopCoroutine(breaked);
+                }
+            }
+
+            // Comment: 역장이 파괴 상태가 아닐 때, 역장 수복 코루틴 실행
+            else if (!isBreaked)
+            {
+                while (durability < 5)
+                {
+                    Coroutine recovery = StartCoroutine(RecoveryShield());
+            
+                    if (durability >= 5)
+                    {
+                        StopCoroutine(recovery);
+                    }
+                }
+            }
+        }
+    }
+    
+
+    
+
+    // Comment: 역장 파괴 쿨다운 코루틴
     IEnumerator ShieldCoolDown()
     {
         Debug.Log("역장 비활성화 카운트");
         // 2초간 역장 활성화 불가
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSecondsRealtime(2.0f);
 
-
-        // 2초 뒤 RecoveryShield 코루틴 실행
+        
+        // Comment: 2초 뒤 RecoveryBreakedShield 코루틴 실행
         Coroutine recovery = StartCoroutine(RecoveryBreakedShield());
-
-        //ToDo : 코루틴 종료해야함
+        
+        if (durability >= 5)
+        {
+            StopCoroutine(recovery);
+        }
     }
 
+    // Comment: 파괴된 역장 수복 코루틴
     IEnumerator RecoveryBreakedShield()
     {
         Debug.Log("역장 복구 시작");
         // 역장 활성화 불가 + 3초 후 역장 수리 완료
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSecondsRealtime(3.0f);
         isBreaked = false;
         Debug.Log("역장 복구 완료");
         durability = 5;
         Debug.Log(shield.GetComponent<LJH_Shield>().durability);
         
-        //ToDo : 코루틴 종료해야함
-        
+    }
+
+    // Comment: 역장 수리 코루틴
+    // ToDo: 내구도 회복 시간 문의해야함
+    IEnumerator RecoveryShield()
+    {
+        Debug.Log("실드 회복 시작(비파괴)");
+        yield return new WaitForSecondsRealtime(1f);
+        durability += 1;
+
     }
 }
