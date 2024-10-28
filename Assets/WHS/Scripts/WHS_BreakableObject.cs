b1901f0101c5ff83bda709f16241f71f5ba7b503
whs1997 <whs1997@naver.com>
2024-10-28 13:30:26 +0900

    Feat: 아이템 생성 변경
    오브젝트 파괴 후 아이템 생성 및 파편 제거

--- Assets/WHS/Scripts/WHS_BreakableObject.cs
+++ Assets/WHS/Scripts/WHS_BreakableObject.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class WHS_BreakableObject : MonoBehaviour
 {
-    // 파괴할 오브젝트에 Fracture 컴포넌트와 함께 추가
+    // 파괴할 오브젝트에 Fracture 컴포넌트와 함께 추가해서 사용
 
-    [SerializeField] GameObject itemPrefab;
+    [SerializeField] GameObject itemPrefab; // 아이템 프리팹 등록
 
-    void Start()
+    private void Start()
     {
-        // FractureManager의 딕셔너리에 등록해서, 파편화 후 제거되게 만듬
-        FractureManager.Instance.GetFractureObject(gameObject);
+        // FractureManager의 딕셔너리에 등록해 오브젝트를 파괴하고 아이템 생성
+        WHS_FractureManager.Instance.GetFractureObject(gameObject, itemPrefab);
     }
 
-    // 파괴 후 비활성화 된 오브젝트의 자리에 itemPrefab 생성
-    private void OnDisable()
-    {
-        if (itemPrefab != null)
-        {
-            Instantiate(itemPrefab, transform.position, Quaternion.identity);
-        }
-    }
+    // OnDestroy()로 아이템을 생성하니 Fracture 후 n초뒤 삭제되서 아이템 생성이 늦게 되는 문제
+    // OnDisable()로 생성해도 동작에 문제가 생겨서 FractureManager에서 생성하게함
 
+    /*
+    private void OnDestroy()
+    {
+        Vector3 dropPos = obj.transform.position + new Vector3(0, 1.5f, 0);
+        Instantiate(itemPrefab, dropPos, Quaternion.identity); // 오브젝트가 파괴된 자리에 1.5높이에 아이템 생성
+    }
+    */
 }
