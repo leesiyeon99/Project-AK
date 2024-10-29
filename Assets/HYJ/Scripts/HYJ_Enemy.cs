66167fb72c154766833119c9612a4599d854903c
realhyj <9k9k9k9k@naver.com>
2024-10-29 15:50:31 +0900

    Feat : 몬스터 피격 데미지를 위한 세팅 및 설정
    몬스터 피격 데미지를 위한 세팅과 설정입니다.
    데미지 텍스트를 표기하려 합니다.(진행 중)

--- Assets/HYJ/Scripts/HYJ_Enemy.cs
+++ Assets/HYJ/Scripts/HYJ_Enemy.cs
@@ -9,19 +9,25 @@ public class HYJ_Enemy : MonoBehaviour
     [SerializeField] GameObject monster;
     [SerializeField] MonsterType monsterType;
     [SerializeField] MonsterAttackType monsterAttackType;
-    [SerializeField] float monsterAttackPower;
-    [SerializeField] float monsterAttackRange;
-    [SerializeField] float setBossHp;
 
+    [SerializeField] float monsterShieldAtkPower;
+    [SerializeField] float monsterHpAtkPower;
+    [SerializeField] float monsterAttackRange;
+
+    [SerializeField] float setBossHp;
     [SerializeField] float monsterHp;
     [SerializeField] float monsterMoveSpeed;
     [SerializeField] float playerDistance;
 
+    [SerializeField] float aniTime;
+
     [SerializeField] Animator monsterAnimator;
     private bool isAttack;
+    private bool nowAttack;
 
     //------------------------임의 변수---------------------------//
     public float playerAttackPower=20;
+    private bool isShield;
 
     public enum MonsterType
     {
@@ -39,6 +45,8 @@ public class HYJ_Enemy : MonoBehaviour
     void Start()
     {
         isAttack = false;
+        nowAttack = false;
+        
         MonsterSetHp();
         MonsterSetAttackRange();
     }
@@ -91,18 +99,19 @@ public class HYJ_Enemy : MonoBehaviour
             }
             else if(playerDistance <= monsterAttackRange && isAttack==false && monsterHp > 0) //Comment : 플레이어가 몬스터의 공격범위로 들어왔을 때
             {
+                monsterAnimator.SetBool("Run Forward", false);
                 isAttack = true;
                 StartCoroutine(MonsterAttackCo());
             }
             else
             {
-                monsterAnimator.SetBool("Run Forward",false);
+
             }
         }
     }
 
     // Comment : 온트리거 엔터를 이용하여 총알과의 충돌 여부를 확인, 충돌 시, 캐릭터의 공격력 or 무기의 공격력이 완료되면 몬스터 피격 함수를 진행시킨다.
-    private void MonsterTakeMamage()
+    private void MonsterTakeDamageCalculation()
     {
         // Comment : 현재는 임의로 playerAttackPower 변수를 활용하여 작성했다.
         if (monsterType == MonsterType.Nomal)
@@ -123,17 +132,22 @@ public class HYJ_Enemy : MonoBehaviour
     {
         monsterAnimator.SetTrigger("Attack");
         Debug.Log("몬스터 공격");
-        // TODO : 여기서 몬스터가 공격 했다는걸 알려줄 수 있는 방식을 만들기
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(aniTime);
+        nowAttack = true;
+        nowAttack = false;        
+        yield return new WaitForSeconds(1f);
         isAttack = false;
     }
 
+    // Comment : 다른 오브젝트와 충돌 시
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bullet"))
         {
-            MonsterTakeMamage();
+            MonsterTakeDamageCalculation();
             // TODO : 충돌 지점을 받기
+
+            //other.transform.position -> 충돌 지점
             // TODO : 받은 충돌 지점이 머리 / 몸통 어디인지 판별하기
             // TODO : 몸통이면 흰색, 머리면 빨간색으로 데미지 표기
         }
@@ -152,7 +166,26 @@ public class HYJ_Enemy : MonoBehaviour
         }
     }
 
-    // TODO : 몬스터 등급에 따른 이펙트 만들기
+    // Comment : 적 등급 설정에 따른 태그 변경
+    private void MonsterTagSet(MonsterType monsterType)
+    {
+        if(monsterType == MonsterType.Nomal)
+        {
+            //gameObject.tag = "Enemy";
+        }
+        else if (monsterType == MonsterType.Elite)
+        {
+            //gameObject.tag = "EliteEnemy";
+        }
+        else if(monsterType == MonsterType.Boss)
+        {
+            //gameObject.tag = "Boss";
+        }
+    }
 
-    // TODO : 몬스터 등급에 따라 태그명 바꾸기
+    // TODO : 몬스터 등급에 따른 이펙트 만들기
+    private void MonsterEffect()
+    {
+
+    }
 }
