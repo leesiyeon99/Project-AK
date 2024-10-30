601946c60e0904143c8d39d81a97b32a3c2e61e4
Z1758 <timeofwitch@gmail.com>
2024-10-30 15:34:55 +0900

    Feat : 스크립터블 준비

---
+++ Assets/KSW/Scripts/Scriptable/WeaponData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "WeaponData", menuName = "ScriptableObjects/WeaponData", order = int.MaxValue)]
+public class WeaponData : ScriptableObject
+{
+    
+}
