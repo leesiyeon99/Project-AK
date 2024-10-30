2c06947f0ce7d1a24ae07ce08fc747de2d210546
leesiyeon99 <qsqs9090@naver.com>
2024-10-30 10:38:59 +0900

    Fix : 몬스터 인디케이터 공격력에 따라 UI표시 변경
    enemy쪽과 연계해서 작업해야 하는건 머지 후 진행

--- Assets/LSY/LSY_Scripts/MonsterDetectionScript/MonsterCount.cs
+++ Assets/LSY/LSY_Scripts/MonsterDetectionScript/MonsterCount.cs
@@ -6,12 +6,26 @@ using UnityEngine.UI;
 
 public class MonsterCount : MonoBehaviour
 {
+    [Header("몬스터 숫자 카운트 UI")]
     [SerializeField] TextMeshProUGUI textMeshPro;
+
+    [Header("몬스터 인디케이터 UI 이미지")]
     [SerializeField] Image monsterDetectionImageUI;
+
+    [Header("몬스터 인디케이터 배경 이미지")]
     [SerializeField] Image monsterCountBackgroundImage;
+
+    [Header("일반 몬스터 아이콘 이미지")]
     [SerializeField] Image normalEnemyIcon;
+
+    [Header("강한 몬스터 아이콘 이미지")]
     [SerializeField] Image strongEnemyIcon;
+
+   // public HYJ_Enemy lsy_Enemy;
+    private float test_monsterAttackPower = 5;
+
     int score = 0;
+    private Coroutine StrongAttackRoutine;
 
     private void Start()
     {
@@ -22,51 +36,136 @@ public class MonsterCount : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            score++;
-            textMeshPro.text = score.ToString();
-            if (other.GetComponent<UnitToScreenBoundary>() != null)
-            other.GetComponent<UnitToScreenBoundary>().isActiveUI = true;
-            normalEnemyIcon.gameObject.SetActive(true);
-            strongEnemyIcon.gameObject.SetActive(false);
+            // if (other.GetComponent<HYJ_Enemy>.isdied = true)
+            // test_monsterAttackPower 부분을 용진님 몬스터 공격력을 받아와서 if 작성
+            if (test_monsterAttackPower >= 3) //other.GetComponent<HYJ_Enemy>.공격력 >= 3 
+            {
+                //강한 적
+            }
+            else
+            {
+                //일반 적
+            }
+            HandleEnemyEntry(other);
         }
 
         if (other.gameObject.CompareTag("EliteEnemy"))
         {
-            score++;
-            textMeshPro.text = score.ToString();
-            if (other.GetComponent<UnitToScreenBoundary>() != null)
-                other.GetComponent<UnitToScreenBoundary>().isActiveUI = true;
-
-            monsterDetectionImageUI.color = Color.white;
-            monsterCountBackgroundImage.color = Color.yellow;
-            normalEnemyIcon.gameObject.SetActive(false);
-            strongEnemyIcon.gameObject.SetActive(true);
+            HandleStrongEnemyEntry(other);
         }
-        
+
+        // 몬스터가 죽었을 때는 exit한 것과 같은 코드 구현하도록 이벤트 발생
+        var enemy = other.GetComponent<HYJ_Enemy>();
+        if (enemy != null)
+        {
+            //enemy.OnEnemyDied.AddListener(HandleEnemyExitOnDeath);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            score--;
-            textMeshPro.text = score.ToString();
-            // 몬스터가 죽기전까지는 계속 화면상에 들어오면 UI 이미지 활성화? 
-            //other.GetComponent<UnitToScreenBoundary>().isActiveUI = false;
-            normalEnemyIcon.gameObject.SetActive(true);
-            strongEnemyIcon.gameObject.SetActive(false);
+            HandleEnemyExit();
         }
-        
+
         if (other.gameObject.CompareTag("EliteEnemy"))
         {
-            score--;
-            textMeshPro.text = score.ToString();
+            HandleStrongEnemyExit();
+        }
+    }
 
-            monsterDetectionImageUI.color = Color.yellow;
-            monsterCountBackgroundImage.color= Color.white;
+    private void HandleEnemyEntry(Collider other)
+    {
+        score++;
+        UpdateScoreText();
+
+        if (other.GetComponent<UnitToScreenBoundary>() != null)
+            other.GetComponent<UnitToScreenBoundary>().isActiveUI = true;
+
+        normalEnemyIcon.gameObject.SetActive(true);
+        strongEnemyIcon.gameObject.SetActive(false);
+    }
+
+    private void HandleStrongEnemyEntry(Collider other)
+    {
+            score++;
+            UpdateScoreText();
+
+            if (other.GetComponent<UnitToScreenBoundary>() != null)
+                other.GetComponent<UnitToScreenBoundary>().isActiveUI = true;
+
+
+
+            monsterDetectionImageUI.color = Color.white;
+            monsterCountBackgroundImage.color = Color.yellow;
+
+            StrongAttackRoutine = StartCoroutine(StrongMonsterAttack());
+    }
+
+    private void HandleEnemyExit()
+    {
+        score--;
+        UpdateScoreText();
+        normalEnemyIcon.gameObject.SetActive(true);
+        strongEnemyIcon.gameObject.SetActive(false);
+    }
+
+    private void HandleStrongEnemyExit()
+    {
+        score--;
+        UpdateScoreText();
+
+        monsterDetectionImageUI.color = Color.yellow;
+        monsterCountBackgroundImage.color = Color.white;
+
+        normalEnemyIcon.gameObject.SetActive(true);
+        strongEnemyIcon.gameObject.SetActive(false);
+
+        if (StrongAttackRoutine != null)
+        {
+            StopCoroutine(StrongAttackRoutine);
+            StrongAttackRoutine = null;
+        }
+    }
+
+    private void HandleEnemyExitOnDeath(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Enemy") || collider.gameObject.CompareTag("EliteEnemy"))
+        {
+            HandleEnemyExit(); 
+        }
+    }
+
+    private void UpdateScoreText()
+    {
+        textMeshPro.text = score.ToString();
+    }
+
+    private IEnumerator StrongMonsterAttack()
+    {
+        WaitForSeconds delay = new WaitForSeconds(0.2f);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 3f)
+        {
+            normalEnemyIcon.gameObject.SetActive(false);
+            strongEnemyIcon.gameObject.SetActive(true);
+            yield return delay;
+
             normalEnemyIcon.gameObject.SetActive(true);
             strongEnemyIcon.gameObject.SetActive(false);
+            yield return delay;
+
+            elapsedTime += 0.4f;
         }
 
+        // 3초 뒤 강한 몬스터의 공격이 끝나면 모든 상태 초기화
+        monsterDetectionImageUI.color = Color.white;
+        monsterCountBackgroundImage.color = Color.white;
+        normalEnemyIcon.gameObject.SetActive(true);
+        strongEnemyIcon.gameObject.SetActive(false);
+        yield break;
     }
+
 }
