2b548a6ae90af6be257f0a54cc2c7e2785c42abd
realhyj <9k9k9k9k@naver.com>
2024-10-31 14:57:01 +0900

    Feat : 몬스터 피격 시, 데미지 텍스트 구현

--- Assets/HYJ/Scripts/HYJ_EnemyHitPoint.cs
+++ Assets/HYJ/Scripts/HYJ_EnemyHitPoint.cs
@@ -1,15 +1,30 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class HYJ_EnemyHitPoint : MonoBehaviour
 {
+
     [SerializeField] HYJ_Enemy enemy;
     [SerializeField] bool weak;
+
+    [Header("데미지 텍스트 설정")]
+    [SerializeField] public GameObject canvas;
+    [SerializeField] public Text damageText;  
  
     private void Awake()
     {
         enemy = GetComponentInParent<HYJ_Enemy>();
     }
-    
+
+    private void Update()
+    {
+        damageText.transform.position = Camera.main.WorldToScreenPoint(transform.position+new Vector3(0,1,0));
+
+
+    }
+
     public bool TakeDamage(float damage)
     {
         if (enemy.HitFlag == false)
@@ -18,15 +33,14 @@ public class HYJ_EnemyHitPoint : MonoBehaviour
             if (weak)
             {
                 Debug.Log("약점");
-                enemy.MonsterTakeDamageCalculation(damage * 1.5f);
-                
+                enemy.MonsterTakeDamageCalculation(damage * 2f);
             }
             else
             {
                 Debug.Log("일반");
                 enemy.MonsterTakeDamageCalculation(damage);
             }
-
+            DamageText(weak, damage);
             enemy.HitFlag = true;
             enemy.StartHitFlagCoroutine();
 
@@ -36,8 +50,6 @@ public class HYJ_EnemyHitPoint : MonoBehaviour
         {
             return false;
         }
-
-      
     }
 
     public bool GetHitFlag()
@@ -45,4 +57,41 @@ public class HYJ_EnemyHitPoint : MonoBehaviour
         return enemy.HitFlag;
     }
     
+
+    public void DamageText(bool isWeak, float damage)
+    {
+        // 몬스터 위에 데미지 폰트 생성
+        // 데미지 set damge로 설정
+        // 데미지 color 설정 (약점이면 빨강/아니면 하얀색)
+        StartCoroutine(OnDamageText(isWeak, damage));
+    }
+
+    public IEnumerator OnDamageText(bool isWeak, float damage)
+    {
+        if (isWeak)
+        {
+            damage = damage * 2f;
+            damageText.fontSize = 70;
+            //damageText 굵게
+            damageText.text = "<b>"+damage.ToString()+"</b>";
+        }
+        else if (!isWeak)
+        {
+            damageText.fontSize = 60;
+            //damageText 굵지 않게
+            damageText.text = damage.ToString();
+        }
+        canvas.SetActive(true);
+        
+        damageText.color = Color.HSVToRGB(255, (enemy.monsterHp / enemy.monsterSetHp) * 255, (enemy.monsterHp / enemy.monsterSetHp) * 255);
+        
+        for (int i = damageText.fontSize ; i >= 30; i--)
+        {
+            damageText.fontSize = i;
+            yield return new WaitForFixedUpdate(); // 다음 FixedUpdte까지 기다림
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+    }
 }
