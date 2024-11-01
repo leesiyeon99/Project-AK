d3485e4bd61aa20c9cdd8b002704fba938898381
realhyj <9k9k9k9k@naver.com>
2024-11-01 14:50:57 +0900

    Feat : 보스 구현

--- Assets/HYJ/Scripts/HYJ_Boss_Stage1.cs
+++ Assets/HYJ/Scripts/HYJ_Boss_Stage1.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class HYJ_Boss_Stage1 : MonoBehaviour
 {
+    [Header("보스 설정")]
+    [SerializeField] GameObject monster;
     [SerializeField] float nowHp;
     [SerializeField] float SetHp;
     //[SerializeField] float 
+    [SerializeField] public float monsterShieldAtkPower;
+    [SerializeField] public float monsterHpAtkPower;
+    [SerializeField] public float monsterMoveSpeed;
 
+    [Header("임의 변수")]
+    [SerializeField] public bool hitFlag;
+    public bool HitFlag { get { return hitFlag; } set { hitFlag = value; } }
 
-
+    Coroutine hitFlagCoroutine;
+    WaitForSeconds hitFlagWaitForSeconds = new WaitForSeconds(0.1f);
     private void Start()
     {
         gameObject.tag = "Boss";
+        nowHp = SetHp;
+    }
+
+    private void Update()
+    {
+        
     }
 }
