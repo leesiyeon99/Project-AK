998948e4af6abc7f510df792bebb035915bf9617
whs1997 <whs1997@naver.com>
2024-11-02 23:28:34 +0900

    Fix: curStage 분리

---
+++ Assets/WHS/Scripts/WHS_StageIndex.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WHS_StageIndex
+{
+    public static int curStage { get; set; } = 1;
+    public static int maxStage { get; } = 5;
+}
