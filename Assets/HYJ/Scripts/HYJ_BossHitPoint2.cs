1c91176dfdba3163b645155597de0e1b9fd1b890
LeeJaeHo-96 <towpam123@gmail.com>
2024-11-05 12:18:44 +0900

    fix: 보스 텍스트 주석 처리

--- Assets/HYJ/Scripts/HYJ_BossHitPoint2.cs
+++ Assets/HYJ/Scripts/HYJ_BossHitPoint2.cs
@@ -59,8 +59,8 @@ public class HYJ_BossHitPoint2 : MonoBehaviour
         // 데미지 color 설정 (약점이면 빨강/아니면 하얀색)
         Debug.Log(isWeak);
         Debug.Log(damage);
-        StartCoroutine(OnDamageText(isWeak, damage));
-        damageText.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 2, 0));
+        //StartCoroutine(OnDamageText(isWeak, damage));
+        //damageText.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 2, 0));
     }
 
     public IEnumerator OnDamageText(bool isWeak, float damage)
